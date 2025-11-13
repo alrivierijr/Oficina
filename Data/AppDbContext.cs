f00221f27ddf2805e7c74d024db17b58d3873f46
Antonio L Rivieri Jr <junior@menezessgarbi.com.br>
2025-11-13 17:08:05 -0300

    Incluindo Acesso ao Banco

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace Oficina.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        // Exemplo de entidade
+        public DbSet<Cliente> Clientes { get; set; } = default!;
+
+    }
+
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+    }
+
+
+}
